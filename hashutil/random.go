a0f888c01c610a18c982c27e15fadccc777c62de
arikkfir <arik@kfirs.com>
2023-06-02 22:15:02 +0300

    Add hashutil package

---
+++ hashutil/random.go
@@ -0,0 +1,13 @@
+package hashutil
+
+import "math/rand"
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func NewHash(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
